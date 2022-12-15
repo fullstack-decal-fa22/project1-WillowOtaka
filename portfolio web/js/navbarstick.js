19f0db8e744a31add719425db26ec3ee0540c803
WillowOtaka <willowotaka@berkeley.edu>
2022-12-15 09:58:32 -0800

    update

---
+++ portfolio web/js/navbarstick.js
@@ -0,0 +1,13 @@
+//navbar stuff.
+window.onscroll = function() {stick()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stick() {
+    if(window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
